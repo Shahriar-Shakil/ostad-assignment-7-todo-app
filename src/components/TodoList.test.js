f85e9014b104b31deb8f8e499da5c8fe70a4c59d
shahriar <amivaloasi>
2023-08-02 23:28:07 +0600

    todolist all test added

--- src/components/TodoList.test.js
+++ src/components/TodoList.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import TodoList from "./Todolist";
-
+// 1
 test("render TodoList Component", () => {
   render(<TodoList />);
   const headerElement = screen.getByText("Todo List - Module 7 assignment");
   expect(headerElement).toBeInTheDocument();
 });
-
+// 2
 test("add a new task to the list", () => {
   render(<TodoList />);
   // Find the input element and the "Add Task" button
@@ -24,4 +24,30 @@ test("add a new task to the list", () => {
 
   const newTask = screen.getByText("New Task");
   expect(newTask).toBeInTheDocument();
+});
+// 3
+test("deletes a task from list", () => {
+  render(<TodoList />);
+
+  // add a task to the list
+  const taskInput = screen.getByPlaceholderText("Enter a new task");
+  const addButton = screen.getByText("Add Task");
+  fireEvent.change(taskInput, { target: { value: "Task to be deleted" } });
+  fireEvent.click(addButton);
+
+  // Find the "Delete" button
+  const deleteButton = screen.getByText("Delete");
+
+  //click the "Delete" button
+  fireEvent.click(deleteButton);
+  // Check if the task is removed from the list
+  const deletedTask = screen.queryByText("Task to be deleted");
+  expect(deletedTask).not.toBeInTheDocument();
+});
+//4
+// Test: Displays 'No tasks added yet.' when no tasks are added
+test("displays 'No tasks added yet.' when no tasks are added", () => {
+  render(<TodoList />);
+  const noTasksMessage = screen.getByText("No tasks added yet.");
+  expect(noTasksMessage).toBeInTheDocument();
 });
